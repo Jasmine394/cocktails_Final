de3e4c9d050e66497c51f955fad5d5fced600a04
Jasmine394 <64162482+Jasmine394@users.noreply.github.com>
2021-03-01 02:24:30 -0800

    more

---
+++ backend/src/main/java/com/aim/cocktails/cocktails_Final/measure/Measure.java
@@ -0,0 +1,36 @@
+package com.aim.cocktails.cocktails_Final.measure;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "measure")
+public class Measure {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "measure_id")
+    private Integer id;
+
+    private String measure;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+}
